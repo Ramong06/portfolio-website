b591d24517132c45413205411f41688138279543
RG-Mac <ramong5406@gmail.com>
2021-03-14 22:54:42 -0500

    changed background, added navbar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { HeroContainer } from './HeroElements';
+import Navbar from "../Navbar";
 
 const Hero = () => {
     // const [isOpen, setIsOpen] = useState(false);
@@ -10,12 +10,8 @@ const Hero = () => {
     // }
     return (
         <HeroContainer>
-            <h1>THIS IS MY HOME PAGE</h1>
-            <Link to="/resume">
-                <button>Resume</button>
-            </Link>
-            {/* <Navbar toggle={toggle} />
-            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar />
+            {/* <Sidebar isOpen={isOpen} toggle={toggle} />
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Greatest Pizza Ever</HeroH1>
