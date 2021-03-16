89aa5cd92ff68906ef95db423c1d50fd5ce62357
RG-Mac <ramong5406@gmail.com>
2021-03-16 16:32:10 -0500

    adding typewriter effect

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import "./styles.css";
 import { Container, Row, Col } from "react-bootstrap";
 import Navbar from "../Navbar";
+const words = ["Developer.", "Creator.", "Problem Solver."];
 
 const Hero = () => {
-    // const [isOpen, setIsOpen] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [subIndex, setSubIndex] = useState(0);
+    const [blink, setBlink] = useState(true);
+    const [reverse, setReverse] = useState(false);
 
-    // const toggle = () => {
-    //     setIsOpen(!isOpen);
-    // }
+    // Typing Effect
+    useEffect(() => {
+        if (index === words.length) return;
+
+        if (subIndex === words[index].length + 1 &&
+            index !== words.length - 1 && !reverse) {
+            setReverse(true);
+            return;
+        }
+        
+        if (subIndex === 0 && reverse) {
+            setReverse(false);
+            setIndex((prev) => prev + 1);
+            return;
+        }
+
+        const timeout = setTimeout(() => {
+            setSubIndex((prev) => prev + (reverse ? -1 : 1));
+        }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 :
+            150, parseInt(Math.random() * 350)));
+        
+        return () => clearTimeout(timeout);
+    }, [subIndex, index, reverse]);
+
     return (
         <div className="hero">
             <Container fluid="md">
                 <Navbar />
                 <Row className="pt-5 justify-content-md-center">
                     <div className="hero-text">
-                        <h1 className="typing__header">I am Ramon Garza III</h1>
-                        <p>And I'm a Web Developer</p>
+                        <h1 className="typing__header">Ramon Garza III</h1>
+                        <p>I AM A</p>
                     </div>
                 </Row>
                 <Row className="pt-3 justify-content-center" id="homeButtons">
