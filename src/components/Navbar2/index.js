5b357c223e0ad8ee3d2ef37505677de02e0d1094
RG-Mac <ramong5406@gmail.com>
2021-03-31 11:09:04 -0500

    changed navbar and navbar collapse on home page

--- src/components/Navbar2/index.js
+++ src/components/Navbar2/index.js
@@ -7,13 +7,17 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 const Navbar2 = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar className="navbar" collapseOnSelect expand="lg" variant="dark">
                 <Navbar.Brand href="#home">LOGO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav className="ml-auto">
+                    
+                    <Nav className="ml-auto">
+                    <Nav.Link href="#deets">More deets</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                    </Nav.Link>
                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -21,13 +25,8 @@ const Navbar2 = () => {
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
-                    </Nav>
-                    <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-                    </Nav.Link>
-                    </Nav>
+                        </Nav>
+                        </Nav>
                 </Navbar.Collapse>
             </Navbar>
         </div>
