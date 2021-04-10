c2d8519fda55f6c63b81dc0b8e1f20a483311044
RG-Mac <ramong5406@gmail.com>
2021-04-10 01:22:18 -0500

    removed old navbar

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import * as AiIcons from "react-icons/ai";
-import * as IoIcons from "react-icons/io";
+// import React from 'react'
+// import * as AiIcons from "react-icons/ai";
+// import * as IoIcons from "react-icons/io";
 
-export const SidebarData = [
-    {
-        title: "Home",
-        path: "/",
-        icon: <AiIcons.AiFillHome />,
-        className: "nav-text"
-    },
-    {
-        title: "Portfolio",
-        path: "/portfolio",
-        icon: <IoIcons.IoMdPerson />,
-        className: "nav-text"
-    },
-    {
-        title: "Resumé",
-        path: "/resume",
-        icon: <IoIcons.IoIosPaper />,
-        className: "nav-text"
-    },
-    // {
-    //     title: "Gallery",
-    //     path: "/gallery",
-    //     icon: <IoIcons.IoMdPhotos />,
-    //     className: "nav-text"
-    // },
-    {
-        title: "Contact",
-        path: "/contact",
-        icon: <IoIcons.IoIosContact />,
-        className: "nav-text"
-    },
-]
+// export const SidebarData = [
+//     {
+//         title: "Home",
+//         path: "/",
+//         icon: <AiIcons.AiFillHome />,
+//         className: "nav-text"
+//     },
+//     {
+//         title: "Portfolio",
+//         path: "/portfolio",
+//         icon: <IoIcons.IoMdPerson />,
+//         className: "nav-text"
+//     },
+//     {
+//         title: "Resumé",
+//         path: "/resume",
+//         icon: <IoIcons.IoIosPaper />,
+//         className: "nav-text"
+//     },
+//     // {
+//     //     title: "Gallery",
+//     //     path: "/gallery",
+//     //     icon: <IoIcons.IoMdPhotos />,
+//     //     className: "nav-text"
+//     // },
+//     {
+//         title: "Contact",
+//         path: "/contact",
+//         icon: <IoIcons.IoIosContact />,
+//         className: "nav-text"
+//     },
+// ]
